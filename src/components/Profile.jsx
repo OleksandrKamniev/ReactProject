a23463d67136136bb4b9463705a76949bf8e6eb7
Oleksandr <105547071+OleksandrKamniev@users.noreply.github.com>
2022-10-09 08:41:33 +0300

    Add files via upload

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+    return (<div className='content'>
+        <div>
+            <img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2016/08/featured.png?auto=format&q=60&w=2060&h=1158.75&fit=crop&crop=faces'></img>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>New post</div>
+        </div>
+        <div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+    </div>)
+}
+export default Profile;
